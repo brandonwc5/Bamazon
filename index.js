28f66e6a84523c0fff89b1b9d0948fd2abf28d4d
brandonwc5 <brandonwc5@yahoo.com>
2018-01-31 16:13:39 -0600

    Adding basic logic

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var inquirer = require('inquirer');
 var mySql = require('mySql');
 
 var connection = mySql.createConnection({
-	host: "localHost";
-	port: 8080;
-	user: "root";
-	password: "";
-	dataBase: "Bamazon";
+	host: "localHost",
+	port: 3380,
+	user: "root",
+	password: "",
+	dataBase: "Bamazon"
 })
 
 connection.connect(function(err, result){
@@ -15,3 +15,38 @@ connection.connect(function(err, result){
 	}
 	start();
 })
+
+function start(){
+	inquirer.prompt({
+		name: "itemID",
+		type: "input",
+		message: "What is the ID of the product you would like to purchase?"
+	}, 
+	{
+		name: "howMany",
+		type: "input",
+		message: "How many would you like to purchase?"
+	}).then(function(answer){
+		if(answer.itemID===""){
+			console.log("We need an item ID.");
+			start();
+		} else {
+			findID();
+		} 
+	})
+}
+start();
+function findID(answer){
+  console.log("Finding items that match that ID...\n");
+  var query = connection.query(
+  	"Select * from Bamazon", 
+    function(err, res) {
+      console.log(res.affectedRows + " products updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
